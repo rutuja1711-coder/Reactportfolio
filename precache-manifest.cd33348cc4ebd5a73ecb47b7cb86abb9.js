a1ffe06e9b70100642b00ec6296918b47d96c57a
rutuja1711-art <rutujagajare00@gmail.com>
2025-05-15 12:37:20 +0530

    Updates

---
+++ precache-manifest.cd33348cc4ebd5a73ecb47b7cb86abb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d909ca318a58f6e6c59516d8adb455",
+    "url": "/Reactportfolio/index.html"
+  },
+  {
+    "revision": "fa73fa1adf2cf37d3f5d",
+    "url": "/Reactportfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "27dff76e84aab56d43cc",
+    "url": "/Reactportfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "fa73fa1adf2cf37d3f5d",
+    "url": "/Reactportfolio/static/js/2.1f1f9c1d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Reactportfolio/static/js/2.1f1f9c1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27dff76e84aab56d43cc",
+    "url": "/Reactportfolio/static/js/main.16b6d537.chunk.js"
+  },
+  {
+    "revision": "353314554ccb089cff84",
+    "url": "/Reactportfolio/static/js/runtime-main.a88261e2.js"
+  }
+]);
